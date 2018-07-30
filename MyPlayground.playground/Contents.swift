2c8308f2eaf2be6d7f95b44a3f082ffaf1369187
teradakunio <terada@lclco.com>
2018-07-30 11:35:37 +0900

    swiftの基礎　課題１

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+//
+//let name = "Steve"
+//var age = 56
+//print(name)
+//print(age)
+//
+//var a:String
+//var b:String
+//a = "ごきげん"
+//b = "いかが？"
+//print(a + b)
+//
+//let one = 1
+//let two = 2
+//print(one + two)
+//
+//let saisho = "おはよう"
+//let tsugi = "ごさいます"
+//print(saisho + tsugi)
+//
+//var c:Int = 0
+//print(c)
+//c = c + 1
+//print(c)
+//
+//var d:Int = 10, e:Int = 3
+//print(d / e)
+//var f:Float = 10, g:Float = 3
+//print(f / g)
+//var h:Double = 10, i:Double = 3
+//print(h / i)
+
+var j:Int = 10, k:Int = 3
+print(j % k)
+
+let l:Int = 1
+print( l > 2)
+
+var phoneNumber: String? = "090"
+if phoneNumber != nil {
+    print("The phone number is existing\(phoneNumber!)")
+}
+
+let steve = (name:"Steve Jobs", age: 56)
+print(steve.age)
+
+
