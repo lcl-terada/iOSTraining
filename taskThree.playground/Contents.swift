ef33b5ed2d9ac9c05fc9904933a9365169ce46ab
teradakunio <terada@lclco.com>
2018-08-17 10:47:58 +0900

    強参照と弱参照

---
+++ taskThree.playground/Contents.swift
@@ -0,0 +1,16 @@
+class ParentObject {
+}
+
+class ChildObject {
+    var val: ParentObject? = ParentObject()  //強参照
+//    weak var val: ParentObject? = ParentObject()  //弱参照
+    init(parentObj: ParentObject) {
+        val = parentObj
+    }
+}
+
+var parentObj: ParentObject? = ParentObject()
+var childObj: ChildObject? = ChildObject(parentObj: parentObj!)
+
+parentObj = nil
+print(childObj?.val as Any)
