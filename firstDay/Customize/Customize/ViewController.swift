d476694d78d2428602f6ef9e2dd227ff57e285ea
teradakunio <terada@lclco.com>
2018-08-20 15:40:11 +0900

    taskFourの内容を移行

--- firstDay/Customize/Customize/ViewController.swift
+++ firstDay/Customize/Customize/ViewController.swift
@@ -1,15 +1,15 @@
 //
 //  ViewController.swift
-//  taskFour
+//  Customize
 //
-//  Created by Kunio Terada on 2018/08/17.
+//  Created by Kunio Terada on 2018/08/20.
 //  Copyright © 2018年 Kunio Terada. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,12 +18,12 @@ class ViewController: UIViewController {
         sampleView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         self.view.addSubview(sampleView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
