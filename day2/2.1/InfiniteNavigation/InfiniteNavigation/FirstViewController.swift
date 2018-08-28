6b172c385aa68396b59f9ca3e63628d46f19cc06
teradakunio <terada@lclco.com>
2018-08-28 17:14:07 +0900

    NavigationVar表示

---
+++ day2/2.1/InfiniteNavigation/InfiniteNavigation/FirstViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let pushButton = UIButton()
+        
+        pushButton.setTitle("Push", for: .normal)
+        pushButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20.0)
+        pushButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        pushButton.addTarget(self, action: #selector(pushButtonTapped), for: .touchUpInside)
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(pushButton)
+        pushButton.translatesAutoresizingMaskIntoConstraints = false
+        pushButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        pushButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        pushButton.heightAnchor.constraint(equalToConstant: 150.0).isActive = true
+        pushButton.widthAnchor.constraint(equalToConstant: 200.0).isActive = true
+        
+    }
+    
+    @objc func pushButtonTapped(_ sender: UIButton) {
+        navigationController?.pushViewController(self, animated: true)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
