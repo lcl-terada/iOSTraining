425afbb8682620b6a1b8b22c4ca4242a8cb53b71
teradakunio <terada@lclco.com>
2018-09-03 12:30:00 +0900

    tableView FirstView設定完了

---
+++ day2/2.5/UITableView/UITableView/FirstViewController.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forHeaderFooterViewReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(tableView)
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.estimatedRowHeight = 1000
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
